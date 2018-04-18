acc2f4f65d8e79d83089351ccf5e4b9b869797ef
speterson96 <speterson96@student.cvtc.edu>
2018-04-18 20:26:51 +0000

    removed extra comments on code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,6 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_from_auth_hash(env["omniauth.auth"])
     session[:user_id] = @user.id
     redirect_to root_path, notice: "Signed In"
-    #TODO remove follwoing comments
-   # auth= request.env["omniauth.auth"]
-   # session[:omniauth] = auth.except('extra')
-   # user = User.from_omniauth(env["omniauth.auth"])
-   # session[:user_id] = user.id
-   # redirect_to root_path, notice: "SIGNED IN"
   end
 
   def destroy
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-#TODO remove following comments
-  #Jamie
- # provider :google_oauth2, '159543473336-6cmp85jvdil71kgloggcf2cp7ed77efm.apps.googleusercontent.com', '05VBysvCPq9bJnL22Dn7yYEh', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-  #Sam
   provider :google_oauth2, '575868889255-7lqrm0arq6mniostv3o6d7i69ds1b3v5.apps.googleusercontent.com', 'BUPUqP0EuNBW-a-B0PNglvbh', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-  #Skyler
- # provider :google_oauth2, '786807918743-jikhuln47kb3n1lhttphhaumil6i7eiv.apps.googleusercontent.com', 'OSVLeb9HuuvriP2LbYcucrDP', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-  #Alex
-  
 end
