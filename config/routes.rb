703a710b7c47bc8333a6fb5479a0dfe82aeae3b1
speterson96 <speterson96@student.cvtc.edu>
2018-05-09 20:42:47 +0000

    fixed problem with linking add item manually and add item to inventory

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'login_system#signup', as: 'signup'
   get '/userInfo', to: 'login_system#userInfo', as: 'userInfo'
   get '/reports', to: 'inventory#reports', as: 'reports'
-  
-  get '/addItemToInventory', to: 'inventory#addItemManually', as: 'addItemToInventory'
+  get '/addItemToInventory', to: 'inventory#addItem', as: 'addItemToInventory'
+  get '/addItemToInventoryManually', to: 'inventory#addItemManually', as: 'addItemToInventoryManually'
   #Google Auth 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
